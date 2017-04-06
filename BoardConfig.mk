49bc36a54f1aa49fb79a799898874ef1e1ec87e0
ccsysadmin <sysadmin@cepucyber.com>
2017-04-07 04:22:12 +0700

    Add files via upload

---
+++ BoardConfig.mk
@@ -0,0 +1,107 @@
+#
+# Copyright (C) 2016 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_PATH := device/lenovo/P70
+-include vendor/lenovo/P70/BoardConfigVendor.mk
+TARGET_PROVIDES_INIT_RC := true
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 := armeabi-v7a
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_VARIANT := generic
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a53
+TARGET_CPU_SMP := true
+TARGET_USES_64_BIT_BINDER := true
+TARGET_IS_64_BIT := true
+ARCH_ARM_HAVE_TLS_REGISTER := true
+
+# COMPILER FLAGS
+#TARGET_GLOBAL_CFLAGS +=
+#TARGET_GLOBAL_CPPFLAGS +=
+
+# Platform
+TARGET_BOARD_PLATFORM := mt6752
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := mt6752
+#TARGET_NO_BOOTLOADER := false
+
+# Kernel
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_HEADER_ARCH := arm64
+TARGET_PREBUILT_KERNEL := device/lenovo/P70/Image.gz
+
+# Boot image
+BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.selinux=permissive androidboot.bootdevice=mtk-msdc.0
+BOARD_KERNEL_BASE := 0x40080000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00000000 --ramdisk_offset 0x03f80000 --tags_offset 0x0df80000
+
+# MKImage
+TARGET_MKIMAGE := $(LOCAL_PATH)/mkimage
+TARGET_USE_BUILT_BOOTIMAGE := true
+TARGET_KMODULES := true
+
+# File systems
+BOARD_HAS_LARGE_FILESYSTEM := true
+#BOARD_HAS_NO_REAL_SDCARD := false
+BOARD_SUPPRESS_EMMC_WIPE := true
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+
+# Partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x01000000
+BOARD_CACHEIMAGE_PARTITION_SIZE := 0x1a800000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x01000000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x80000000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x42000000
+BOARD_FLASH_BLOCK_SIZE := 0x20000
+
+# Recovery
+TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/ramdisk/recovery.fstab
+
+# TWRP recovery
+RECOVERY_VARIANT := twrp
+
+# TWRP specific build flags
+TW_THEME := portrait_hdpi
+TW_NO_SCREEN_BLANK := true
+RECOVERY_SDCARD_ON_DATA := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+TW_BRIGHTNESS_PATH := /sys/devices/platform/leds-mt65xx/leds/lcd-backlight/brightness
+TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone1/temp
+#TW_NO_USB_STORAGE := false
+TW_EXCLUDE_SUPERSU := true
+TW_INCLUDE_NTFS_3G := true
+TW_IGNORE_MISC_WIPE_DATA := true
+
+# Asian region languages
+TW_EXTRA_LANGUAGES := true
+
+# Encryption support
+TW_INCLUDE_CRYPTO := true
+#TARGET_HW_DISK_ENCRYPTION := true
+TARGET_KEYMASTER_WAIT_FOR_QSEE := true
+
+# Debug flags
+TWRP_INCLUDE_LOGCAT := true
---
+++ vendorsetup.sh
@@ -0,0 +1,2 @@
+add_lunch_combo cm_P70-eng
+add_lunch_combo cm_P70-userdebug
